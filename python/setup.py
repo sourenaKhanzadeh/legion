8bc326be7b85e9cf099d3877bddfc4219b45e3bc
sourena <sourena.khanzadeh@gmail.com>
2025-02-15 21:58:44 -0500

    [FIX] Path Added for the Cuda file

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+import os
+
+# Define the path to the C++ CUDA files
+cuda_path = os.path.abspath("../C++/cuda")
 
 setup(
     name="gpu_compute",
     ext_modules=[
-        CUDAExtension("gpu_compute", ["gpu_compute.cu"]),
+        CUDAExtension(
+            "gpu_compute",
+            [os.path.join(cuda_path, "gpu_compute.cu")],  # Locate CUDA file correctly
+        ),
     ],
     cmdclass={"build_ext": BuildExtension},
 )
